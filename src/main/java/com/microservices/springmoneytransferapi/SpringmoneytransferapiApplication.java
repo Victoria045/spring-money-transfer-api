98b447b64a8e833979dce3275c14a4684a91d11b
Victoria Beryl <vakoth@safaricom.co.ke>
2023-04-04 08:43:09 +0300

    initializing the application

---
+++ src/main/java/com/microservices/springmoneytransferapi/SpringmoneytransferapiApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.springmoneytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmoneytransferapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmoneytransferapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/springmoneytransferapi/SpringmoneytransferapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.springmoneytransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmoneytransferapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
