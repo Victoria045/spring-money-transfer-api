daad708e79e6dcad192f8686f468fb91847e716b
Victoria Beryl <vakoth@safaricom.co.ke>
2023-04-05 23:07:59 +0300

    prevent cross site scripting by adding a basic auth

---
+++ src/main/java/com/microservices/springmoneytransferapi/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.microservices.springmoneytransferapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Autowired
+    private ReactiveUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/api/**").authenticated()
+                .anyExchange().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("development")
+                .password(passwordEncoder().encode("dXNlcjpwYXNzd29yZA=="))
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+}
